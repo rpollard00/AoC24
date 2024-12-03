5923943d44a9dc9adb645d6fdb9a17eb017a707a
Reese Pollard <rpollard@gmail.com>
2024-12-02 17:59:17 -0800

    I have no idea what I am doing (F# edition)

---
+++ 1/1_1/Program.fs
@@ -0,0 +1,21 @@
+﻿open System
+// For more information see https://aka.ms/fsharp-console-apps
+let lines = IO.File.ReadLines("input.txt")
+
+// read each line
+let (first_list, second_list) =
+    lines
+    |> Seq.map (fun str ->
+        let parts = str.Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries)
+        (int parts.[0], int parts.[1]))
+    |> Seq.toList
+    |> List.unzip
+
+let sorted_first_list = first_list |> List.sort
+let sorted_second_list = second_list |> List.sort
+
+let diff_list = List.zip sorted_first_list sorted_second_list
+
+let res = diff_list |> Seq.map (fun (a, b) -> Math.Abs(a - b)) |> Seq.sum
+
+printfn "%d" res
---
+++ 1/1_2/Program.fs
@@ -0,0 +1,30 @@
+﻿open System
+// For more information see https://aka.ms/fsharp-console-apps
+let lines = IO.File.ReadLines("input.txt")
+
+// read each line
+let (first_list, second_list) =
+    lines
+    |> Seq.map (fun str ->
+        let parts = str.Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries)
+        (int parts.[0], int parts.[1]))
+    |> Seq.toList
+    |> List.unzip
+
+let sorted_first_list = first_list |> List.sort
+let sorted_second_list = second_list |> List.sort
+
+// iterate over the second list, create a hashmap where the number is the key and the value is the number of times that value appears
+let counts = sorted_second_list |> Seq.countBy id |> Map.ofSeq
+// ok for each input in the first list, we need to determine how many times it appears in the second list
+// iterate over the first list, for each element in the first list
+let res =
+    sorted_first_list
+    |> Seq.map (fun k ->
+        match Map.tryFind k counts with
+        | Some count -> k * count
+        | None -> 0)
+    |> Seq.sum
+
+
+printfn "%d" res
