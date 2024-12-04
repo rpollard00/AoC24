d60e455b7a61a0e13566d08c3c942e49fb7fa0a4
Reese Pollard <rpollard@gmail.com>
2024-12-03 23:25:15 -0800

    Day 1 remove some stuff

--- 1/1_2/Program.fs
+++ 1/1_2/Program.fs
@@ -1,8 +1,7 @@
 ﻿open System
-// For more information see https://aka.ms/fsharp-console-apps
+
 let lines = IO.File.ReadLines("input.txt")
 
-// read each line
 let (first_list, second_list) =
     lines
     |> Seq.map (fun str ->
@@ -14,10 +13,7 @@ let (first_list, second_list) =
 let sorted_first_list = first_list |> List.sort
 let sorted_second_list = second_list |> List.sort
 
-// iterate over the second list, create a hashmap where the number is the key and the value is the number of times that value appears
 let counts = sorted_second_list |> Seq.countBy id |> Map.ofSeq
-// ok for each input in the first list, we need to determine how many times it appears in the second list
-// iterate over the first list, for each element in the first list
 let res =
     sorted_first_list
     |> Seq.map (fun k ->
