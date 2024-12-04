979f3277fc54879b350029ad02713b71cd63a4cc
Reese Pollard <rpollard@gmail.com>
2024-12-03 23:25:00 -0800

    Day 4

---
+++ 4/4_1/Program.fs
@@ -0,0 +1,46 @@
+﻿open System.IO
+
+let grid = File.ReadLines("input.txt") |> Seq.toArray
+
+let width = grid.[0].Length - 1
+let height = grid.Length - 1
+
+let xmas = [ 'M'; 'A'; 'S' ]
+let n = [ (-1, 0); (-2, 0); (-3, 0) ]
+let s = [ (1, 0); (2, 0); (3, 0) ]
+let e = [ (0, 1); (0, 2); (0, 3) ]
+let w = [ (0, -1); (0, -2); (0, -3) ]
+let nw = [ (-1, -1); (-2, -2); (-3, -3) ]
+let ne = [ (-1, 1); (-2, 2); (-3, 3) ]
+let sw = [ (1, -1); (2, -2); (3, -3) ]
+let se = [ (1, 1); (2, 2); (3, 3) ]
+
+let card = [ n; s; e; w; nw; ne; sw; se ]
+let xmas_card = card |> List.map (fun dir -> List.zip xmas dir)
+
+let x_s_collector = ResizeArray<int * int>()
+
+for r in 0..height do
+    for c in 0..width do
+        if grid.[r].[c] = 'X' then
+            // printfn "Pos (%d, %d) is %c" r c grid.[r].[c]
+            x_s_collector.Add((r, c))
+
+let x_s = x_s_collector |> List.ofSeq
+
+let is_char (x: char) ((r, c): int * int) =
+    if r > width || c > height then false
+    elif r < 0 || c < 0 then false
+    elif grid.[r].[c] = x then true
+    else false
+
+
+let is_xmas dir sr sc =
+    dir |> List.forall (fun (v, (r, c)) -> is_char v (sr + r, sc + c))
+
+let sum_xmas xr xc =
+    xmas_card |> List.sumBy (fun d -> if is_xmas d xr xc then 1 else 0)
+
+let res = x_s |> List.sumBy (fun (xr, xc) -> sum_xmas xr xc)
+
+printfn "%d" res
---
+++ 4/4_2/Program.fs
@@ -0,0 +1,60 @@
+﻿open System.IO
+
+let grid = File.ReadLines("input.txt") |> Seq.toArray
+
+let width = grid.[0].Length - 1
+let height = grid.Length - 1
+
+// ok so instead we need to find the 'A's
+// then from a given A we need to check the diagonals
+// we can have a couple permutations
+// check nw and se -> if nw is S then se must be M or if nw is M then se must be S
+// check ne and sw -> if ne is S then sw must be M or if nw is S then se must be M
+// both the above make a valid xmas
+//let xmas = [ 'M'; 'A'; 'S' ]
+//let n = [ (-1, 0); (-2, 0); (-3, 0) ]
+//let s = [ (1, 0); (2, 0); (3, 0) ]
+//let e = [ (0, 1); (0, 2); (0, 3) ]
+//let w = [ (0, -1); (0, -2); (0, -3) ]
+let nw = (-1, -1)
+let ne = (-1, 1)
+let sw = (1, -1)
+let se = (1, 1)
+type card = list<char * (int * int)>
+type cards = list<card>
+let l_r_a: card = [ 'M', nw; 'S', se ]
+let l_r_b: card = [ 'S', nw; 'M', se ]
+let r_l_a: card = [ 'M', ne; 'S', sw ]
+let r_l_b: card = [ 'S', ne; 'M', sw ]
+
+let l_card: cards = [ l_r_a; l_r_b ]
+let r_card: cards = [ r_l_a; r_l_b ]
+
+let a_s_collector = ResizeArray<int * int>()
+
+for r in 0..height do
+    for c in 0..width do
+        if grid.[r].[c] = 'A' then
+            a_s_collector.Add((r, c))
+
+let a_s = a_s_collector |> List.ofSeq
+
+let is_char (x: char) ((r, c): int * int) =
+    if r > width || c > height then false
+    elif r < 0 || c < 0 then false
+    elif grid.[r].[c] = x then true
+    else false
+
+let is_half_mas sr sc (cards: cards) =
+    cards
+    |> List.exists (fun card -> card |> List.forall (fun (v, (r, c)) -> is_char v (sr + r, sc + c)))
+
+let is_xmas sr sc =
+    if is_half_mas sr sc l_card && is_half_mas sr sc r_card then
+        1
+    else
+        0
+
+let res = a_s |> List.sumBy (fun (xr, xc) -> is_xmas xr xc)
+
+printfn "%d" res
