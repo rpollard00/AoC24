2a589708fa79c4300f21b1bc31d171246455aafb
Reese Pollard <rpollard@gmail.com>
2024-12-06 23:06:32 -0800

    Easy optimization

--- 7/7_2/Program.fs
+++ 7/7_2/Program.fs
@@ -1,6 +1,35 @@
 ﻿open System.IO
 open System
 
+
+let list_prt lst =
+    lst |> List.iter (fun v -> printfn "%d" v)
+
+let concat_int v1 v2 =
+    let v1str = string v1
+    let v2str = string v2
+
+    let strv = v1str + v2str
+    int64 strv
+
+let (^^) v1 v2 = concat_int v1 v2
+
+let rec test_ops target (values: list<int64>) =
+    match values with
+    | h :: _ when h > target -> false
+    | [] -> target = 0L
+    | [ last ] -> target = last
+    | v1 :: v2 :: rest ->
+        let add_res = v1 + v2
+        let mul_res = v1 * v2
+        let concat_res = v1 ^^ v2
+
+        test_ops target (add_res :: rest)
+        || test_ops target (mul_res :: rest)
+        || test_ops target (concat_res :: rest)
+
+let stopWatch = System.Diagnostics.Stopwatch.StartNew()
+
 let input =
     File.ReadLines("input.txt")
     |> Seq.map (fun e -> e.Trim().Split(':', StringSplitOptions.RemoveEmptyEntries))
@@ -18,32 +47,6 @@ let lines =
         (int64 result, values))
     |> List.ofSeq
 
-let list_prt lst =
-    lst |> List.iter (fun v -> printfn "%d" v)
-
-let concat_int v1 v2 =
-    let v1str = string v1
-    let v2str = string v2
-
-    let strv = v1str + v2str
-    int64 strv
-
-let (^^) v1 v2 = concat_int v1 v2
-
-let rec test_ops target (values: list<int64>) =
-    match values with
-    | [] -> target = 0L
-    | [ last ] -> target = last
-    | v1 :: v2 :: rest ->
-        let add_res = v1 + v2
-        let mul_res = v1 * v2
-        let concat_res = v1 ^^ v2
-
-        test_ops target (add_res :: rest)
-        || test_ops target (mul_res :: rest)
-        || test_ops target (concat_res :: rest)
-
-
 let valid =
     lines
     |> List.map (fun (res, vals) ->
@@ -56,3 +59,5 @@ let valid =
     |> List.sum
 
 printfn "%d" valid
+stopWatch.Stop()
+printfn "%f" stopWatch.Elapsed.TotalMilliseconds
